a6ab8f400cd63e0062e1a3384c912dc27e9b6f44
Maryam Heshmati <mairheshmati@gmail.com>
2020-04-07 15:12:02 -0700

    finished wave 1- make API calls to get the list of all pets. Finished wave 2, make API call to find a pet given the pet ID

--- src/adaPets.js
+++ src/adaPets.js
@@ -9,25 +9,47 @@ const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
 const listPets = () => {
-  // Fill out as part of Wave 1.
+  axios.get(BASE_URL)
+  .then((response)=> {
+    // console.log(response.data)
+    setResult(response.data)
+  })
+  .catch((error) => {
+    // console.log(`This is the error message ${error}`)
+    setError(`This is the error message ${error}`)
+  })
 };
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
-    
   }
 
-  // Fill out as part of Wave 2.
+  axios.get(`${BASE_URL}${selectedPetId}`)
+  .then((response) => {
+    console.log(response.data)
+    setResult(response.data)
+  })
+  .catch((error) => {
+    setError(`select show details ${error}`)
+  })
 };
 
 const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
-    
   }
 
-  // Fill out as part of Wave 3.
+  axios.delete(`${BASE_URL}${selectedPetId}`)
+  .then((response) => {
+    console.log(response.data)
+    setResult(response)
+  })
+  .catch((error) => {
+    setError(`select show details ${error}`)
+  })
+
+
 };
 
 const addPet = (petInfo) => {
@@ -41,3 +63,4 @@ module.exports = {
   removePet,
   addPet
 };
+
