211511bc5b76f71541dad7bdd93439a095786ff7
Emily Cowan <e.anderson8977@gmail.com>
2020-04-07 15:12:06 -0700

    completed wave 3

--- src/adaPets.js
+++ src/adaPets.js
@@ -9,29 +9,65 @@ const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
 const listPets = () => {
-  // Fill out as part of Wave 1.
+// Making the request for a list of all pets
+  axios.get(BASE_URL)
+    .then((response) => {
+      setResult(response.data)
+    })
+    .catch((error) => {
+      setError('An Error has occured: ${error}')
+    });
 };
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
-    
-  }
-
-  // Fill out as part of Wave 2.
+  } else {
+    axios.get(BASE_URL + selectedPetId)
+    .then((response) => {
+      console.log(response)
+      setResult(response.data)
+    })
+    .catch((error) => {
+      setError('Error: Request failed with status code 404')
+    })
+  };
 };
+
+//  else {
+//   params = {
+//     "id": selectedPetId
+//   }
+//   axios.get(BASE_URL, {
+//     params: params
+//   })
+//   .then((response) => {
+//     setResult(response.data[0])
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//     setError('Error: Request failed with status code 404')
+//   })
+// }
 
 const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
-    
-  }
+  } else {
+    axios.delete(BASE_URL + selectedPetId)
+    .then((response) => {
+      setResult('Request to remove pet successful.')
+    })
+    .catch((error) => {
+      setError('Request to remove failed.')
+    })
+  };
 
   // Fill out as part of Wave 3.
 };
 
 const addPet = (petInfo) => {
-  // Fill out as part of Wave 4.
+  
 };
 
 // Use Node-style exports to export functions for tests and main.
