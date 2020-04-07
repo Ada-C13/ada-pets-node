825aa271cbe0bc6c347d040faa2cb9427af19b20
denisseai <msdenisseanaya@gmail.com>
2020-04-07 12:29:14 -0700

    wave 1: List pets

--- src/adaPets.js
+++ src/adaPets.js
@@ -10,7 +10,14 @@ const BASE_URL = 'http://localhost:3000/pets/';
 // Option functions.
 const listPets = () => {
   // Fill out as part of Wave 1.
-};
+  axios.get(BASE_URL)
+  .then((response) => {
+    setResult(response.data);
+  })
+  .catch(() => {
+    setError("Something went wrong");
+  });
+}
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
