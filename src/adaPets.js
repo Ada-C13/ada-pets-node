599cc6e5914771aa91669347b2ab53db1e52d676
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-07 14:43:17 -0700

    Passes 2/3 tests.

--- src/adaPets.js
+++ src/adaPets.js
@@ -24,10 +24,29 @@ const listPets = () => {
   });
 };
 
+// We now want to be able to look at individual pets. Like in Slack CLI we're going to want to select a given pet before we ask for details on one. To do this we're going to fill out a few functions that we're creating within a closure. There is a provided function selectPet that will prompt you for a pet's id and save it.
+
+// To do this you will need to fill out the petDetails function. This will need to make a call to the Pets API and should setResult and object with details for the pet and should call setError with an error message if the request fails.
+
+// Function to complete
+// showDetails
+// setResult should be passed the Object that represents the pet.
+// setError should be passed an error message. (You may need to write this message.)
+
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
-    
+  }
+  else if (selectedPetId) {
+    axios.get(BASE_URL + selectedPetId)
+    .then((response) => {
+      //ASSIGNMENT REQ: setResult should be passed the array of pets.
+      setResult(response.data);
+    })
+    .catch((error) => {
+      //ASSIGNMENT REQ: setError should be passed an error message. (You may need to write this message.)
+      setError("Request failed.");
+    });
   }
 
   // Fill out as part of Wave 2.
