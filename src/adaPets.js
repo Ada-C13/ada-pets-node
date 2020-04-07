0f34ff5befaf7c0afdcb83d533d9de76ec1dfb0a
Nikki Vaughan <vnikki13@gmail.com>
2020-04-07 13:55:50 -0700

    Finished wave1 with passing tests

--- src/adaPets.js
+++ src/adaPets.js
@@ -9,7 +9,9 @@ const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
 const listPets = () => {
-  // Fill out as part of Wave 1.
+  axios.get(BASE_URL)
+  .then((result) => {setResult(result.data)})
+  .catch((error) => {setError("Local Host down")})
 };
 
 const showDetails = (selectedPetId) => {
