ba270569c38665d79569bdc9e0d8288321dd97c4
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-07 14:56:50 -0700

    Passes 1/2 Wave 4 tests.

--- src/adaPets.js
+++ src/adaPets.js
@@ -41,17 +41,7 @@ const showDetails = (selectedPetId) => {
   }
 };
 
-// Once a pet is adopted we don't still want to show it on the list. (We don't want people fighting over the same pet after all 😉.)
-
-// To do this fill out the removePet function. This will need to call to the Pets API to remove the pet. This should setResult with a success message if this works and should call setError with an error message if the request fails.
-
-// You can run the Wave 3 tests with jest test/wave3.test.js. Once these are passing move on to Wave 4.
-
-// Function to complete
-// removePet
-// setResult should be passed a success message. (You may need to write this message.)
-// setError should be passed an error message. (You may need to write this message.)
-
+//WAVE 3: To do this fill out the removePet function. This will need to call to the Pets API to remove the pet. This should setResult with a success message if this works and should call setError with an error message if the request fails.
 const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
@@ -67,11 +57,18 @@ const removePet = (selectedPetId) => {
       setError("404: Request to remove pet failed.");
     });
   }
-  // Fill out as part of Wave 3.
 };
 
+//WAVE 4: To do this fill out the addPet function. This will need to call the Pets API to add the pet. This should setResult with a success message if this works and should call setError with an error message if the request fails.
 const addPet = (petInfo) => {
-  // Fill out as part of Wave 4.
+  axios.post(BASE_URL + petInfo)
+  .then((response) => {
+    setResult(response.data);
+  })
+  .catch((error) => {
+    //ASSIGNMENT REQ: setError should be passed an error message. (You may need to write this message.)
+    setError("404: Request to add pet failed.");
+  });
 };
 
 // Use Node-style exports to export functions for tests and main.
