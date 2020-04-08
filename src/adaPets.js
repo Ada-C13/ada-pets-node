be428ed3c203b5af09e301558fdac705ec59d8ed
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-07 20:56:40 -0700

    Refactor

--- src/adaPets.js
+++ src/adaPets.js
@@ -15,59 +15,49 @@ const listPets = () => {
     setResult(response.data);
   })
   .catch(error => {
-    setError(`Error occured: ${error}`);
+    setError(`Error: ${error.message}`);
 })
 };
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
-  
-    // Wave 2.
+  // Wave 2.
   } else {
     axios.get(BASE_URL + selectedPetId)
-  .then(response => {
-    setResult(response.data)
-  })
-  .catch(error => {
-    setError(`Error: ${error.message}`);
-  })
-  }
+    .then(response => {
+      setResult(response.data);
+    })
+    .catch(error => {
+      setError(`Error: ${error.message}`);
+    })
+  };
 };
 
 const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
-    
-  }
-
-  // Fill out as part of Wave 3.
-  axios.delete(BASE_URL + selectedPetId)
-  .then(response => {
-    setResult(response.data)
-  })
-  .catch(error => {
-    setError(`Can't remove a selected pet: ${error.message}`);
-  })
-
+  } else {
+  // Wave 3.
+    axios.delete(BASE_URL + selectedPetId)
+    .then(response => {
+      setResult(response.data);
+    })
+    .catch(error => {
+      setError(`Can't remove a selected pet: ${error.message}`);
+    })
+  };
 };
 
 const addPet = (petInfo) => {
   // Wave 4.
-  axios.post(BASE_URL, {
-    name: petInfo.name,
-    species: petInfo.species,
-    age: petInfo.age,
-    owner: petInfo.owner,
-    about: petInfo.about    
-  })
+  axios.post(BASE_URL, petInfo)
   .then(response => {
-    setResult(response.data)
+    setResult(response.data);
   })
   .catch(error => {
     setError(`Failed to add a new pet: ${error.message}`);
   })
-
 };
 
 // Use Node-style exports to export functions for tests and main.
