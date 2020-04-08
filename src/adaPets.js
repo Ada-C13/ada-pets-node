a3cdb0ea7c9c32074e6c053965316b550fe990c1
ChelseaC13 <chelhelg@outlook.com>
2020-04-07 21:12:14 -0700

    lost in wave 2

--- src/adaPets.js
+++ src/adaPets.js
@@ -9,15 +9,36 @@ const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
 const listPets = () => {
-  // Fill out as part of Wave 1.
+  axios.get('http://localhost:3000/pets')
+    .then((response) => {
+      let petCollection = []   //I got this how to from Kate P's classroom support slack post
+      
+      response.data.forEach(pet => {
+        petCollection.push({id: pet.id, name: pet.name});
+      })
+    
+      setResult(petCollection);
+    })
+    .catch((error) => {
+      setError(`made up error message for wave one list pets ${error.response}`);
+  });
 };
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
-    setError("You tried to show details for a pet without selecting it!");
-    
+    setError("You tried to show details for a pet without selecting it!"); 
   }
+  const url = `http://localhost:3000/pets/${selectedPetId}`
 
+  axios.get('url')  // how is this added with params
+    .then((response) => {
+      let selectedPetInfo = response 
+      // console.log(response)
+      setResult(selectedPetInfo);
+    })
+    .catch((error) => {
+      setError(error);
+  });
   // Fill out as part of Wave 2.
 };
 
