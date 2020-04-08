c54cc43c90c951a478871affb98d0cc8a92bc69e
Faezeh Ashtiani <faa@Faezehs-MacBook-Pro.local>
2020-04-07 18:27:45 -0700

    passes wave 2

--- src/adaPets.js
+++ src/adaPets.js
@@ -22,23 +22,43 @@ const listPets = () => {
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
-    
-  }
-
+    return;
+  } 
+  axios.get(BASE_URL + selectedPetId)
+  .then((response) => {
+    setResult(response.data);
+  })
+  .catch((error) => {
+    setError(`your request failed with code: ${error.response.status} `)
+  });
   // Fill out as part of Wave 2.
 };
 
 const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
-    
+    return;
   }
+  axios.delete(BASE_URL + selectedPetId)
+  .then((response) => {
 
+  })
+  .catch((error) => {
+
+  });
   // Fill out as part of Wave 3.
 };
 
 const addPet = (petInfo) => {
   // Fill out as part of Wave 4.
+  console.log(petInfo)
+  axios.post(BASE_URL, petInfo)
+  .then((response) => {
+
+  })
+  .catch((error) => {
+    // set(petInfo)
+  });
 };
 
 // Use Node-style exports to export functions for tests and main.
