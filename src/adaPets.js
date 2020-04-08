4d03aca4fe95ace19536c5ffd798deb440585ea1
Nataliya Pogodina <n.pogodina@gmail.com>
2020-04-08 15:27:29 -0700

    Wave 4: filled out addPet()

--- src/adaPets.js
+++ src/adaPets.js
@@ -14,14 +14,14 @@ const listPets = () => {
     setResult(response.data);
   })
   .catch(function (error) {
-    setError("Unable to retrieve pets data. Sorry!")
+    setError("Unable to retrieve pets data. Sorry!");
+    console.log(error.response.status);
   })
 };
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
-    
   }
 
   // Fill out as part of Wave 2.
@@ -30,7 +30,7 @@ const showDetails = (selectedPetId) => {
     setResult(response.data);
   })
   .catch(function (error) {
-    setError(`Command show details about ${selectedPetId} failed with: 404 Not Found. Please, select another pet!`)
+    setError(`Command "show details" about ${selectedPetId} failed with: ${error.response.status} ${error.response.statusText}. Please, select another pet!`)
   })
 };
 
@@ -45,12 +45,19 @@ const removePet = (selectedPetId) => {
     setResult(`Pet ${selectedPetId} successfuly deleted`);
   })
   .catch(function (error) {
-    setError(`Command remove pet ${selectedPetId} failed with: 404 Not Found. Please, select another pet!`)
+    setError(`Command remove pet ${selectedPetId} failed with: ${error.response.status} ${error.response.statusText}. Please, select another pet!`)
   })
 };
 
 const addPet = (petInfo) => {
   // Fill out as part of Wave 4.
+  axios.post('http://localhost:3000/pets/?', petInfo)
+  .then(function (response) {
+    setResult(response.data);
+  })
+  .catch(function (error) {
+    setError("Failed to add a pet")
+  });
 };
 
 // Use Node-style exports to export functions for tests and main.
