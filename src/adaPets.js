dc826ab22c4522cb8cd907c33d3b94adcf74d5c0
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-04-08 11:10:36 -0700

    ada pets waves 1-4

--- src/adaPets.js
+++ src/adaPets.js
@@ -1,37 +1,65 @@
 // Use Node-style imports for dependencies.
-const axios = require('axios');
-const result = require('./result.js');
+const axios = require("axios");
+const result = require("./result.js");
 
 const setResult = result.setResult;
 const setError = result.setError;
 
-const BASE_URL = 'http://localhost:3000/pets/';
+const BASE_URL = "http://localhost:3000/pets/";
 
 // Option functions.
 const listPets = () => {
-  // Fill out as part of Wave 1.
+  axios
+    .get(BASE_URL)
+    .then((response) => {
+      setResult(response.data);
+    })
+
+    .catch((error) => {
+      setError(`There was an error listing pets: ${error.message}`);
+    });
 };
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
-    
+  } else {
+    axios
+      .get(BASE_URL + selectedPetId)
+      .then((response) => {
+        setResult(response.data);
+      })
+      .catch((error) => {
+        setError(`There was an error selecting pet: ${error.message}`);
+      });
   }
-
-  // Fill out as part of Wave 2.
 };
 
 const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
-    
+  } else {
+    axios
+      .delete(BASE_URL + selectedPetId)
+      .then((response) => {
+        setResult("It was successfully removed");
+      })
+      .catch((error) => {
+        setError(`It failed trying to remove  pet: ${error.message}`);
+      });
   }
-
-  // Fill out as part of Wave 3.
 };
 
 const addPet = (petInfo) => {
-  // Fill out as part of Wave 4.
+  axios
+    .post(BASE_URL, petInfo)
+    .then((response) => {
+      console.log("It was successfully added to the list of pets");
+      setResult(response.data);
+    })
+    .catch((error) => {
+      setError(`It fail to add the new pet: ${error}`);
+    });
 };
 
 // Use Node-style exports to export functions for tests and main.
@@ -39,5 +67,5 @@ module.exports = {
   listPets,
   showDetails,
   removePet,
-  addPet
+  addPet,
 };
