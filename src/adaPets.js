173c032b82bca4463418c0994afc6a27da940c6d
ChelseaC13 <chelhelg@outlook.com>
2020-04-08 16:50:16 -0700

    done

--- src/adaPets.js
+++ src/adaPets.js
@@ -5,54 +5,54 @@ const result = require('./result.js');
 const setResult = result.setResult;
 const setError = result.setError;
 
-const BASE_URL = 'http://localhost:3000/pets/';
-
 // Option functions.
 const listPets = () => {
   axios.get('http://localhost:3000/pets')
     .then((response) => {
-      let petCollection = []   //I got this how to from Kate P's classroom support slack post
-      
-      response.data.forEach(pet => {
-        petCollection.push({id: pet.id, name: pet.name});
-      })
-    
-      setResult(petCollection);
+      setResult(response.data);
     })
     .catch((error) => {
-      setError(`made up error message for wave one list pets ${error.response}`);
+      setError('made up error message for wave one list pets');
   });
 };
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!"); 
-  }
-  const url = `http://localhost:3000/pets/${selectedPetId}`
-
-  axios.get('url')  // how is this added with params
-    .then((response) => {
-      let selectedPetInfo = response 
-      // console.log(response)
-      setResult(selectedPetInfo);
+  } else {
+    let url = `http://localhost:3000/pets/${selectedPetId}`
+    axios.get(url)  
+      .then((response) => {
+        setResult(response.data )
+      })
+      .catch((error) => {
+        setError(`${error}`)
     })
-    .catch((error) => {
-      setError(error);
-  });
-  // Fill out as part of Wave 2.
+  };
 };
 
 const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
-    
-  }
-
-  // Fill out as part of Wave 3.
+  } else {
+    let url = `http://localhost:3000/pets/${selectedPetId}`
+    axios.delete(url)  
+      .then(response => {
+        setResult('Here is a successful response');
+      })
+      .catch((error) => {
+        setError(`Your wave 3 selected animal was not removed. Here is the error:${error}`);
+      });
+  };
 };
 
 const addPet = (petInfo) => {
-  // Fill out as part of Wave 4.
+  axios.post('http://localhost:3000/pets',petInfo)
+    .then(response => {
+    setResult(response.data);
+    }).catch(error => {
+    setError(`Adding a pet in wave 4 falied. Here is your error:${error}`);
+  });   
 };
 
 // Use Node-style exports to export functions for tests and main.
