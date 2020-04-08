afa2c7e01b4c91cdd428eb963bb475f7a87d8dd0
dnguye2 <dnguyen1093@gmail.com>
2020-04-07 17:14:22 -0700

    fixed indentation and spacing to make code clean

--- src/adaPets.js
+++ src/adaPets.js
@@ -10,62 +10,48 @@ const BASE_URL = 'http://localhost:3000/pets/';
 // Option functions.
 const listPets = () => {
   axios.get(BASE_URL)
-
-  .then((response) => {
-    setResult(response.data);
-  })
-
-  .catch((error) => {
-    setError(`Response unsuccessful.`);
-  });
+    .then((response) => {
+      setResult(response.data);
+    })
+    .catch((error) => {
+      setError(`Unsuccessful get request.`);
+    });
 };
 
 const showDetails = (selectedPetId) => {
-  axios.get(BASE_URL + selectedPetId)
-
-  .then((response) => {
-    setResult(response.data);
-  })
-
-  .catch(() => {
-    setError(`Request failed with status code 404 and failed to show details and select pet`);
-  });
-
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
   }
+  axios.get(BASE_URL + selectedPetId)
+    .then((response) => {
+      setResult(response.data);
+    })
+    .catch(() => {
+      setError(`Request failed with status code 404 and failed to show details and select pet`);
+    });
 };
 
 const removePet = (selectedPetId) => {
+  if (!selectedPetId) {
+    setError("You tried to remove a pet without selecting it!");
+  }
   axios.delete(BASE_URL + selectedPetId)
     .then(() => {
       setResult('This pet has been removed because it found its forever home!');
     })
-  
     .catch(() => {
       setError(`Request failed with status code 404 and failed to remove and selected pet`);
     });
-  
-  if (!selectedPetId) {
-    setError("You tried to remove a pet without selecting it!");
-  }
 };
 
 const addPet = (petInfo) => {
-  axios.post(BASE_URL, {
-    name: `${petInfo.name}`,
-    species: `${petInfo.species}`,
-    about: `${petInfo.about}`
-  })
-  
-  .then((response) => {
-    setResult(response.data);
-  })
-  
-  .catch((error) => {
-    console.log(error);
-    setError(`Failed to add pet with status code ${error.response.status}.`);
-  })
+  axios.post(BASE_URL, petInfo)
+    .then((response) => {
+      setResult(response.data);
+    })
+    .catch((error) => {
+      setError(`Failed to add pet with status code ${error.response.status}.`);
+    })
 };
 
 // Use Node-style exports to export functions for tests and main.
