f6675c934d47b02d6e4c4d3b134c50c118b852a3
Jocelyn Wang <wangjoc@gmail.com>
2020-04-09 08:02:28 -0700

    passed wave one test (listPets method)

--- test/wave3.test.js
+++ test/wave3.test.js
@@ -1,67 +1,67 @@
-const axios = require('axios'); // Import axios.
-const MockAdapter = require('axios-mock-adapter'); // This is kind of like VCR.
+// const axios = require('axios'); // Import axios.
+// const MockAdapter = require('axios-mock-adapter'); // This is kind of like VCR.
 
-// Import helper functions.
-const {expectResult, expectError} = require('./helper.js');
+// // Import helper functions.
+// const {expectResult, expectError} = require('./helper.js');
 
-// Import our function(s) for testing.
-const adaPets = require('../src/adaPets.js');
+// // Import our function(s) for testing.
+// const adaPets = require('../src/adaPets.js');
 
-const removePet = adaPets.removePet;
+// const removePet = adaPets.removePet;
 
-// This sets the mock adapter on the default instance
-const mock = new MockAdapter(axios);
+// // This sets the mock adapter on the default instance
+// const mock = new MockAdapter(axios);
 
-const fail = (error) => {
-  throw new Error(`Test failed! ${ error }`);
-};
+// const fail = (error) => {
+//   throw new Error(`Test failed! ${ error }`);
+// };
 
-describe('Wave 3', () => {
-  // Set up axios test responses.
-  describe('removePet', () => {
-    it('Can remove a pet', done => {
-      // Arrange.
-      // Set up what we want the API to return for this test.
-      mock.onDelete('http://localhost:3000/pets/3').reply(204);
+// describe('Wave 3', () => {
+//   // Set up axios test responses.
+//   describe('removePet', () => {
+//     it('Can remove a pet', done => {
+//       // Arrange.
+//       // Set up what we want the API to return for this test.
+//       mock.onDelete('http://localhost:3000/pets/3').reply(204);
 
-      // Assertions come first because they need to be ready before the function call.
-      expectResult(
-        () => done() // No assertions.  We just care that it finished.
-      );
+//       // Assertions come first because they need to be ready before the function call.
+//       expectResult(
+//         () => done() // No assertions.  We just care that it finished.
+//       );
 
-      // Act.
-      removePet(3);
-    });
+//       // Act.
+//       removePet(3);
+//     });
 
-    it('sets an error string when there is no selected pet', done => {
-      expectError(error => {
-        // Assert.
-        expect(error.constructor).toBe(String);
-        expect(error).toMatch('remove');
-        expect(error).toMatch('select');
+//     it('sets an error string when there is no selected pet', done => {
+//       expectError(error => {
+//         // Assert.
+//         expect(error.constructor).toBe(String);
+//         expect(error).toMatch('remove');
+//         expect(error).toMatch('select');
 
-        done();
-      });
+//         done();
+//       });
 
-      // Act.
-      removePet();
-    });
+//       // Act.
+//       removePet();
+//     });
 
-    it('sets an error string when the response isn\'t successful', done => {
-      // Arrange.
-      // We want this to fail.
-      mock.onDelete('https://localhost:3000/pets/1000000').reply(404);
+//     it('sets an error string when the response isn\'t successful', done => {
+//       // Arrange.
+//       // We want this to fail.
+//       mock.onDelete('https://localhost:3000/pets/1000000').reply(404);
 
-      expectError(error => {
-        // Assert.
-        expect(error).toMatch(/failed/i);
-        expect(error).toMatch(/remove/i);
+//       expectError(error => {
+//         // Assert.
+//         expect(error).toMatch(/failed/i);
+//         expect(error).toMatch(/remove/i);
 
-        done();
-      });
+//         done();
+//       });
 
-      // Act.
-      removePet(1000000);
-    });
-  });
-});
+//       // Act.
+//       removePet(1000000);
+//     });
+//   });
+// });
--- test/wave4.test.js
+++ test/wave4.test.js
@@ -1,71 +1,71 @@
-const axios = require('axios'); // Import axios.
-const MockAdapter = require('axios-mock-adapter'); // This is kind of like VCR.
+// const axios = require('axios'); // Import axios.
+// const MockAdapter = require('axios-mock-adapter'); // This is kind of like VCR.
 
-// Import helper functions.
-const {expectResult, expectError} = require('./helper.js');
+// // Import helper functions.
+// const {expectResult, expectError} = require('./helper.js');
 
-// Import our function(s) for testing.
-const adaPets = require('../src/adaPets.js');
-const addPet = adaPets.addPet;
+// // Import our function(s) for testing.
+// const adaPets = require('../src/adaPets.js');
+// const addPet = adaPets.addPet;
 
-// This sets the mock adapter on the default instance
-const mock = new MockAdapter(axios);
+// // This sets the mock adapter on the default instance
+// const mock = new MockAdapter(axios);
 
-const fail = (error) => {
-  throw new Error(`Test failed! ${ error }`);
-};
+// const fail = (error) => {
+//   throw new Error(`Test failed! ${ error }`);
+// };
 
-describe('Wave 4', () => {
-  // Set up axios test responses.
-  describe('addPet', () => {
-    it('Can add a pet', done => {
-      const reqData = {
-        name: 'Artemis',
-        species: 'goddess',
-        about: 'Goddess of the hunt.'
-      };
+// describe('Wave 4', () => {
+//   // Set up axios test responses.
+//   describe('addPet', () => {
+//     it('Can add a pet', done => {
+//       const reqData = {
+//         name: 'Artemis',
+//         species: 'goddess',
+//         about: 'Goddess of the hunt.'
+//       };
 
-      // Arrange.
-      // Set up what we want the API to return for this test.
-      mock.onPost(new RegExp('http://localhost:3000/pets/?'), reqData).reply(
-        200,
-        {
-          id: 918,
-          name: 'Artemis',
-          species: 'goddess',
-          about: 'Goddess of the hunt.'
-        }
-      );
+//       // Arrange.
+//       // Set up what we want the API to return for this test.
+//       mock.onPost(new RegExp('http://localhost:3000/pets/?'), reqData).reply(
+//         200,
+//         {
+//           id: 918,
+//           name: 'Artemis',
+//           species: 'goddess',
+//           about: 'Goddess of the hunt.'
+//         }
+//       );
 
-      // Assertions come first because they need to be ready before the function call.
-      expectResult(result => {
-        expect(result.id).not.toBeNull();
-        expect(result.name).toBe('Artemis');
-        expect(result.species).toBe('goddess');
-        expect(result.about).toMatch('hunt');
+//       // Assertions come first because they need to be ready before the function call.
+//       expectResult(result => {
+//         expect(result.id).not.toBeNull();
+//         expect(result.name).toBe('Artemis');
+//         expect(result.species).toBe('goddess');
+//         expect(result.about).toMatch('hunt');
 
-        done();
-      });
+//         done();
+//       });
 
-      // Act.
-      addPet(reqData);
-    });
+//       // Act.
+//       addPet(reqData);
+//     });
 
-    it('sets an error string when the response isn\'t successful', done => {
-      // Arrange.
-      // We want this to fail.
-      mock.onPost(new RegExp('http://localhost:3000/pets/?'), { name: 'Zeus' }).reply(500);
+//     it('sets an error string when the response isn\'t successful', done => {
+//       // Arrange.
+//       // We want this to fail.
+//       mock.onPost(new RegExp('http://localhost:3000/pets/?'), { name: 'Zeus' }).reply(500);
 
-      expectError(error => {
-        // Assert.
-        expect(error).toMatch(/failed/i);
-        expect(error).toMatch(/add/i);
+//       expectError(error => {
+//         // Assert.
+//         expect(error).toMatch(/failed/i);
+//         expect(error).toMatch(/add/i);
 
-        done();
-      });
+//         done();
+//       });
 
-      // Act.
-      addPet({ name: 'Zeus' });
-    });
-  });
-});
+//       // Act.
+//       addPet({ name: 'Zeus' });
+//     });
+//   });
+// });
