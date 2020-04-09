65406b54dff2681cf164034ccd2829f42d9bbc21
Hannah Tekie <hannahtekie@gmail.com>
2020-04-09 17:38:44 -0400

    completed all the waves and all test passed

--- src/adaPets.js
+++ src/adaPets.js
@@ -8,7 +8,14 @@ const setError = result.setError;
 const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
-const listPets = () => {
+const listPets = () => {axios.get('http://localhost:3000/pets/')
+  .then((response) => {
+    setResult(response.data)
+  })
+  .catch((error) => {
+  setError(`Unfortunately your request didn't work: ${error}`)  
+  });
+  
   // Fill out as part of Wave 1.
 };
 
@@ -16,7 +23,14 @@ const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
     
-  }
+  }else {
+  axios.get(BASE_URL + selectedPetId)
+    .then((response) => {
+      setResult(response.data)
+    })
+    .catch((error) => {
+      setError(`${error}`)
+    }) }
 
   // Fill out as part of Wave 2.
 };
@@ -26,11 +40,26 @@ const removePet = (selectedPetId) => {
     setError("You tried to remove a pet without selecting it!");
     
   }
+  axios.delete(BASE_URL + selectedPetId)
+  .then((response) => {
+    setResult(response.data)
+  })
+  .catch((error) => {
+    setError(`Your request to remove a pet failed, please select the right petId!: ${error}`)
+  })
 
   // Fill out as part of Wave 3.
 };
 
-const addPet = (petInfo) => {
+const addPet = (petInfo) => { axios.post(BASE_URL,  petInfo)
+  .then((response) => {
+    setResult(response.data)
+    
+  })
+  .catch((error) => {
+    setError(`your request to add a pet failed, all details to add a new pet is needed: ${JSON.stringify(petInfo)}`)
+
+  })
   // Fill out as part of Wave 4.
 };
 
