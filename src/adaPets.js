ee2d5c15d50a464ee553393f737862e9b8dfd337
Katie <katie.vandervoort@gmail.com>
2020-04-09 23:17:00 -0700

    implemented listPets for wave 1

--- src/adaPets.js
+++ src/adaPets.js
@@ -9,8 +9,15 @@ const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
 const listPets = () => {
-  // Fill out as part of Wave 1.
+  axios.get(BASE_URL)
+    .then((response) => {
+      setResult(response.data)
+    })
+    .catch((error) => {
+      setError("invalid url")
+    })
 };
+
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
