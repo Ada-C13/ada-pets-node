a642d684e580eb8f5377482daea5be3197a8a371
Halahaddad1 <h.m.haddad@gmail.com>
2020-04-10 00:59:31 -0700

    clean up

--- src/adaPets.js
+++ src/adaPets.js
@@ -11,18 +11,14 @@ const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
 const listPets = () => {
-  //  let result = {}
    axios.get(BASE_URL)
   .then((response) => {
-    // handle success
     setResult(response.data);
   })
   .catch((response) => {
-    // handle error
     setError(`List pets failed with a ${response.response} response!`);
   })
   .then(function () {
-    // always executed
     console.log("list pets request is complete!");
   });
 };
@@ -30,26 +26,20 @@ const listPets = () => {
 
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
-    setError("You tried to show details for a pet without selecting it!");
-    
+    setError("You tried to show details for a pet without selecting it!"); 
   }else {
     axios.get(`${BASE_URL}${selectedPetId}`)
     .then((response) => {
-      // handle success
       setResult(response.data);
     })
     .catch((response) => {
-      // handle error
       setError(`show details failed: /${response.response.status}/ `);
       console.log(`show details failed with a ${response.response.status} response!`)
     })
     .then(function () {
-      // always executed
       console.log("show details request is complete!");
     });
-
   }
-
 };
 
 const removePet = (selectedPetId) => {
@@ -58,34 +48,26 @@ const removePet = (selectedPetId) => {
   }else{
      axios.delete(`${BASE_URL}${selectedPetId}`)
     .then((response) => {
-      // handle success
       setResult(response.data);
     })
     .catch((response) => {
-      // handle error
       setError(`Remove pet failed with a ${response.response.status} response! `);
     })
     .then(function () {
-      // always executed
       console.log("remove pets request is complete!");
     });
   } 
 };
 
 const addPet = (petInfo) => {
-  // Fill out as part of Wave 4.
   axios.post(BASE_URL,petInfo)
     .then((response) => {
-      // handle success
-      
       setResult(response.data);
     })
     .catch((response) => {
-      // handle error
       setError(`Add pet failed with a ${response.response.status} response!`);
     })
     .then(function () {
-      // always executed
       console.log("add pet request is complete!");
     });
 
