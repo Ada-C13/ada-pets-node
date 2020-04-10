18ae1a9e488cf3fe2bb44544c3b3554f47e1bcca
Hannah Tekie <hannahtekie@gmail.com>
2020-04-09 21:29:24 -0400

    fixed the indentation

--- src/adaPets.js
+++ src/adaPets.js
@@ -8,13 +8,14 @@ const setError = result.setError;
 const BASE_URL = 'http://localhost:3000/pets/';
 
 // Option functions.
-const listPets = () => {axios.get('http://localhost:3000/pets/')
-  .then((response) => {
-    setResult(response.data)
-  })
-  .catch((error) => {
-  setError(`Unfortunately your request didn't work: ${error}`)  
-  });
+const listPets = () => {
+  axios.get('http://localhost:3000/pets/')
+    .then((response) => {
+      setResult(response.data)
+    })
+    .catch((error) => {
+      setError(`Unfortunately your request didn't work: ${error}`)  
+    });
   
   // Fill out as part of Wave 1.
 };
@@ -22,15 +23,16 @@ const listPets = () => {axios.get('http://localhost:3000/pets/')
 const showDetails = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to show details for a pet without selecting it!");
-    
-  }else {
+  }
+  else {
   axios.get(BASE_URL + selectedPetId)
     .then((response) => {
       setResult(response.data)
     })
     .catch((error) => {
       setError(`${error}`)
-    }) }
+    })
+  }
 
   // Fill out as part of Wave 2.
 };
@@ -38,28 +40,28 @@ const showDetails = (selectedPetId) => {
 const removePet = (selectedPetId) => {
   if (!selectedPetId) {
     setError("You tried to remove a pet without selecting it!");
-    
   }
   axios.delete(BASE_URL + selectedPetId)
-  .then((response) => {
-    setResult(response.data)
-  })
-  .catch((error) => {
-    setError(`Your request to remove a pet failed, please select the right petId!: ${error}`)
-  })
+    .then((response) => {
+      setResult(response.data)
+    })
+    .catch((error) => {
+      setError(`Your request to remove a pet failed, please select the right petId!: ${error}`)
+    })
 
   // Fill out as part of Wave 3.
 };
 
-const addPet = (petInfo) => { axios.post(BASE_URL,  petInfo)
-  .then((response) => {
-    setResult(response.data)
-    
-  })
-  .catch((error) => {
-    setError(`your request to add a pet failed, all details to add a new pet is needed: ${JSON.stringify(petInfo)}`)
+const addPet = (petInfo) => { 
+  axios.post(BASE_URL,  petInfo)
+    .then((response) => {
+      setResult(response.data)
+      
+    })
+    .catch((error) => {
+      setError(`your request to add a pet failed, all details to add a new pet is needed: ${JSON.stringify(petInfo)}`)
 
-  })
+    })
   // Fill out as part of Wave 4.
 };
 
